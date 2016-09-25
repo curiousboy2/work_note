2ebbf1d63c997393e0ab06ffb38ba63116147dc2
curiousboy2 <1197643508@qq.com>
2016-09-25 15:26:22 +0800

    Update logger.py
    自定义错误输出类

--- logger.py
+++ logger.py
@@ -1,19 +1,18 @@
 import logging
 import datetime
-# 创建一个logger
-logger = logging.getLogger('mylogger')
-logger.setLevel(logging.DEBUG)
-# 创建一个handler，用于写入日志文件
-today=datetime.datetime.now().strftime('%Y-%m-%d')
-fh = logging.FileHandler('%s.log' %today)
-fh.setLevel(logging.DEBUG)
-# 再创建一个handler，用于输出到控制台
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
-# 定义handler的输出格式
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-fh.setFormatter(formatter)
-ch.setFormatter(formatter)
-# 给logger添加handler
-logger.addHandler(fh)
-logger.addHandler(ch)
+
+class custom_logger(logging.Logger):
+    def __init__(self,name):
+        super(custom_logger,self).__init__(name)
+        today=datetime.datetime.now().strftime('%Y-%m-%d')
+        fh = logging.FileHandler('%s.log' %today)
+        fh.setLevel(logging.DEBUG)
+        # 再创建一个handler，用于输出到控制台
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.DEBUG)
+        # 定义handler的输出格式
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        fh.setFormatter(formatter)
+        ch.setFormatter(formatter)
+        self.addHandler(fh)
+        self.addHandler(ch)
