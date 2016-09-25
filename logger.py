5714ae2278959c3c87d4415d535f85c2abfaebda
curiousboy2 <1197643508@qq.com>
2016-09-25 15:46:22 +0800

    自定义错误输出类

--- logger.py
+++ logger.py
@@ -1,18 +1,24 @@
 import logging
-import datetime
+from datetime import datetime
 
-class custom_logger(logging.Logger):
+class myLogger(logging.Logger):
     def __init__(self,name):
-        super(custom_logger,self).__init__(name)
-        today=datetime.datetime.now().strftime('%Y-%m-%d')
-        fh = logging.FileHandler('%s.log' %today)
-        fh.setLevel(logging.DEBUG)
-        # 再创建一个handler，用于输出到控制台
-        ch = logging.StreamHandler()
-        ch.setLevel(logging.DEBUG)
-        # 定义handler的输出格式
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        super(myLogger,self).__init__(name)
+        self.setLevel(logging.DEBUG)
+        today=datetime.now().strftime('%Y-%m-%d')
+        fh=logging.FileHandler('d:\\jeff\\log\\{}.log'.format(today))
+        fh.setLevel(logging.ERROR)
+        ch=logging.StreamHandler()
+        ch.setLevel(logging.INFO)
+        formatter=logging.Formatter("%(asctime)s\t\tuser: %(name)s\t\tlevel: %(levelname)s\t\treason: %(message)s")
         fh.setFormatter(formatter)
         ch.setFormatter(formatter)
         self.addHandler(fh)
         self.addHandler(ch)
+
+if __name__ == '__main__':
+    logger=myLogger('jeff')
+    try:
+        f=open('001.txt')
+    except Exception as e:
+        logger.error(e)
